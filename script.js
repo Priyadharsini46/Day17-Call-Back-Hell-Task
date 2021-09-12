214d39b0b898156920ca91567ef82a723a909f3b
Priyadharsini <86102018+Priyadharsini46@users.noreply.github.com>
2021-09-12 19:08:21 +0530

    Day 17- call back hell task
    Display countdown from 10 to 1 on the screen and then display Happy Independence Day on the screen.

---
+++ script.js
@@ -0,0 +1,45 @@
+setTimeout(() => {
+    const letter = document.querySelector(".count");
+    letter.innerText = 10;
+    setTimeout(() => {
+      const letter = document.querySelector(".count");
+      letter.innerText = 9;
+      setTimeout(() => {
+        const letter = document.querySelector(".count");
+        letter.innerText = 8;
+        setTimeout(() => {
+          const letter = document.querySelector(".count");
+          letter.innerText = 7;
+          setTimeout(() => {
+            const letter = document.querySelector(".count");
+            letter.innerText = 6;
+            setTimeout(() => {
+              const letter = document.querySelector(".count");
+              letter.innerText = 5;
+              setTimeout(() => {
+                const letter = document.querySelector(".count");
+                letter.innerText = 4;
+                setTimeout(() => {
+                  const letter = document.querySelector(".count");
+                  letter.innerText = 3;
+                  setTimeout(() => {
+                    const letter = document.querySelector(".count");
+                    letter.innerText = 2;
+                    setTimeout(() => {
+                      const letter = document.querySelector(".count");
+                      letter.innerText = 1;
+                      setTimeout(() => {
+                        const letter = document.querySelector(".count");
+                        letter.innerText = "Happy Independence Day";
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+  
